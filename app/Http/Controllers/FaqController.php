6ea02616c8846db53ecc804982211874325a0337
Naomi674 <norr0001@hz.nl>
2022-02-09 15:35:16 +0100

    fixed indentation and comments

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 class FaqController
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function show()
     {
         return view('faq');
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 class ProfileController
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function show()
     {
         return view('profile');
